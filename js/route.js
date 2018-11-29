ea585c42424457f07025ec80fd1fa56deaed6688
Applebaby <queeniezhang0810@gmail.com>
2018-11-28 21:40:55 -0500

    Add files via upload

--- js/route.js
+++ js/route.js
@@ -371,110 +371,32 @@ app.controller("myCtrl", function ($scope,$uibModal,$interval,$timeout){
         });
     })
     .controller("contactCtrl", function($scope){
-       var C = $('#container-contact'),
-  	  		A = $('#openDiv'),
-  	  		L = $('#letter'),
-           B = $('.button.con'),
-				 H = $('#letter hgroup h2'),
-           F = $('.front'),
-           W = $('#wrapper'),
-  	  		P = $('#contactDiv'),
-           closed = true;
-$(function() {
-  // Handler for .ready() called.
-  $("textarea").text("");
-});
+        $("textarea").text("");
 
-   F.click(function(){
-				C.css({
-					'transition':'all 1s',
-					'transform':'rotateY(180deg)',
-				});
-				A.css({
-					'transition':'all 1s .5s',
-					'transform':'rotateX(180deg)',
-					'z-index': '0'
-				});
-  			W.css({
-             'visibility':'visible'
-				});
-    });
-
-     	// open/close and spin
-    W.click(function() {
-      var message =$.trim($('textarea').val());
-      if (message.length > 0) {//they wrote something in the message
-      var r=confirm("You have not sent your message, would you still like to close the form?");
-      if (r==false)//they don't want to close
-      {
-        return;
-      }
-      else //they do want to close. clear message
-      {
-     document.getElementById("myform").reset();
-      }
-     }
-         if(closed===false){
-         L.css({
-					'transition':'all .7s',
-					'top':'3px',
-					'height':'200px'
-				});
-     		P.css({
-     			'transform':'translateY(0px)'
-     		});
-				F.css({
-     			'transform':'rotateZ(0deg)'
-				});
- 				H.css({
-     			'transition':'all .5s',
-     			'transform':'rotateZ(0deg)'
-				});
-				C.css({
-					'transition':'all 1.2s .95s'
-				});
-				A.css({
-					'transition':'all 1.2s .7s'
-				});
-				H.css({
-					'transition':'all .5s'
-				});
-           document.getElementById("info").innerHTML = "P:123.456.7890";
-          closed=true;
-        }
-        else{
-				C.css({
-					'transition':'all 1s .5s',
-				});
-				A.css({
-					'transition':'all .5s',
-				});
-          closed=false;
-        }
-				C.css({
-					'transform':'rotateY(0deg) rotate(3deg)'
-				});
-				A.css({
-					'transform':'rotateX(0deg)',
-					'z-index': '10'
-				});
-     		W.css({
-            //'transition':'all .5s',
-             'visibility':'hidden'
+        $('.front').click(function(){
+        		$('#container-contact').css({
+        			'transition':'all 1s',
+        			'transform':'rotateY(180deg)',
+        		});
+        		$('#openDiv').css({
+        			'transition':'all 1s .5s',
+        			'transform':'rotateX(180deg)',
+        			'z-index': '0'
+        		});
         });
-     	});
+
      	// Open letter
       $scope.openEnvelope = function(){
-        L.css({
+        $('#letter').css({
 					'transition':'all .5s 1s',
 					'top':'-600px',
 					'height':'550px'
 				});
-     		P.css({
+     		$('#contactDiv').css({
      			'transition':'all 1s',
      			'transform':'translateY(450px)'
      		});
-				H.css({
+				$('#letter hgroup h2').css({
      			'transition':'all 1s',
      			'transform':'rotateZ(180deg)'
 				});
